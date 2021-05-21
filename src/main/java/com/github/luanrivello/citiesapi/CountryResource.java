580f9d074783798a893d43b6c4aabc99a803e88b
Luan Belem <luanrivello@hotmail.com>
2021-05-20 23:40:03 -0300

    aula 8

--- src/main/java/com/github/luanrivello/citiesapi/CountryResource.java
+++ src/main/java/com/github/luanrivello/citiesapi/CountryResource.java
@@ -4,9 +4,12 @@ import com.github.luanrivello.citiesapi.countries.Country;
 import com.github.luanrivello.citiesapi.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -21,6 +24,18 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page) {
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
 }
